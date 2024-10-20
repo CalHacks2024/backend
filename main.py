b2db789ca6f55533a57dccc98e2c78391be9fe54
Adi Poluri <adipoluri@gmail.com>
2024-10-20 04:46:38 -0700

    added langchain

--- main.py
+++ main.py
@@ -7,15 +7,21 @@ import json
 import uuid
 from datetime import datetime, timedelta
 from pydantic import BaseModel
+from groq import Groq
 
 # LLM imports
 from langchain.chains.flare.prompts import PROMPT_TEMPLATE
 from langchain_core.prompts import ChatPromptTemplate
-from openai import OpenAI
+# from openai import OpenAI
 from rag import getDB, generate_data_store
 from consts import CHROMA_PATH
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+# from langchain.llms import OpenAI
+from langchain_openai import ChatOpenAI
+# from langchain.schema.runnable import RunnableSequence
+from langchain_core.runnables import RunnableSequence, RunnableMap
 import asyncio
-
 
 class state:
     doctor_id = 63902541323
@@ -24,6 +30,11 @@ class state:
     dirty = False
     kill = False
     task = None
+    groqSimple = ""
+    groqDetailed = ""
+    questions = ""
+    diagnosis = ""
+    moreResearchRequired = ""
 
     windows = {}
 
@@ -39,10 +50,15 @@ app.add_middleware(
 )
 
 state = state()
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
+# client = OpenAI(
+#     api_key=os.environ.get("OPENAI_API_KEY"),
+# )
+groqClient = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
 )
+
 getDB()
+
 
 
 # Health Check
@@ -60,15 +76,15 @@ def reports(doctor_id: int):
     for filename in os.listdir(directory):
         if filename.endswith('.json'):  # Check for JSON files
             file_path = os.path.join(directory, filename)
-
+            
             try:
                 with open(file_path, 'r') as json_file:
                     data = json.load(json_file)
-
+                    
                     # Check if 'doctor_id' exists in the JSON data
                     if 'doctor_id' in data and data['doctor_id'] == doctor_id:
                         res.append(data)
-
+      
             except json.JSONDecodeError:
                 print(f"{filename}: Error decoding JSON")
             except Exception as e:
@@ -76,17 +92,14 @@ def reports(doctor_id: int):
 
     return {"reports": res}
 
-
 class DeleteReportRequest(BaseModel):
     appointment_id: str
-
 
 @app.delete("/deleteAppointment")
 def deleteAppointment(DeleteReportRequest: DeleteReportRequest):
     # lmao super unsafe but this is an mvp so ¯\_(ツ)_/¯
-    file_path = os.path.join(
-        "./reports/", DeleteReportRequest.appointment_id+".json")
-
+    file_path = os.path.join("./reports/", DeleteReportRequest.appointment_id+".json")
+    
     try:
         # Check if the file exists
         if not os.path.isfile(file_path):
@@ -95,10 +108,9 @@ def deleteAppointment(DeleteReportRequest: DeleteReportRequest):
         # Delete the file
         os.remove(file_path)
         return {"detail": "File deleted successfully"}
-
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
 
 class Report(BaseModel):
     name: str
@@ -109,20 +121,18 @@ class Report(BaseModel):
     social_history: str
     review_of_symptoms: str
 
-
+    
 class UpdateReportRequest(BaseModel):
     doctor_id: int
     appointment_id: str
     date: str
     data: Report
 
-
 @app.post("/updateAppointment")
 def updateAppointment(UpdateReportRequest: UpdateReportRequest):
     # lmao super unsafe but this is an mvp so ¯\_(ツ)_/¯
-    file_path = os.path.join(
-        "./reports/", UpdateReportRequest.appointment_id+".json")
-
+    file_path = os.path.join("./reports/", UpdateReportRequest.appointment_id+".json")
+    
     try:
         # Check if the file exists
         if not os.path.isfile(file_path):
@@ -130,15 +140,15 @@ def updateAppointment(UpdateReportRequest: UpdateReportRequest):
 
         # Delete the file
         os.remove(file_path)
-
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-
+    
     file_name = "./reports/" + UpdateReportRequest.appointment_id + ".json"
     data = {
-        "doctor_id": UpdateReportRequest.doctor_id,
-        "appointment_id": UpdateReportRequest.appointment_id,
-        "date": UpdateReportRequest.date,
+        "doctor_id":UpdateReportRequest.doctor_id,
+        "appointment_id":UpdateReportRequest.appointment_id,
+        "date":UpdateReportRequest.date,
         "data": {
             "name": UpdateReportRequest.data.name,
             "age": UpdateReportRequest.data.age,
@@ -158,10 +168,9 @@ def updateAppointment(UpdateReportRequest: UpdateReportRequest):
     return {"detail": "File updated successfully"}
 
 
-# Spectacles Appointments
+# Spectacles Appointments 
 class DoctorRequest(BaseModel):
     doctor_id: int  # Required field
-
 
 @app.post("/startAppointment")
 async def startAppointment(DoctorRequest: DoctorRequest):
@@ -171,32 +180,30 @@ async def startAppointment(DoctorRequest: DoctorRequest):
     appointment_id = uuid.uuid1()
     state.current_appointment = str(appointment_id)
     state.kill = False
-
-    # Run Agent Handler in new Thread
+    
+    ## Run Agent Handlers in new Thread
     asyncio.create_task(agent_handler())
-    return {"appointment_id": appointment_id}
+    return {"appointment_id":appointment_id}
 
 
 class AppointmentEndRequest(BaseModel):
     doctor_id: int  # Required field
     appointment_id: str
 
-
 @app.post("/endAppointment")
 def endAppointment(AppointmentEndRequest: AppointmentEndRequest):
     if state.doctor_id != AppointmentEndRequest.doctor_id:
-        raise HTTPException(
-            status_code=400, detail="Failure: Doctor ID mismatch")
-
+        raise HTTPException(status_code=400, detail="Failure: Doctor ID mismatch") 
+    
     if state.current_appointment != AppointmentEndRequest.appointment_id:
-        raise HTTPException(
-            status_code=400, detail="Failure: Appointment ID mismatch")
-
+        raise HTTPException(status_code=400, detail="Failure: Appointment ID mismatch") 
+    
     # TODO: Generate data based on current state
+
     data = {
-        "doctor_id": state.doctor_id,
-        "appointment_id": state.current_appointment,
-        "date": datetime.now().strftime("%B %d, %Y"),
+        "doctor_id":state.doctor_id,
+        "appointment_id":state.current_appointment,
+        "date":datetime.now().strftime("%B %d, %Y"),
         "data": {
             "name": "Alan Wang",
             "age": "23",
@@ -215,9 +222,9 @@ def endAppointment(AppointmentEndRequest: AppointmentEndRequest):
             json.dump(data, json_file, indent=4)
     except:
         print(f"{file_name}: Error Writing JSON")
-
+    
     state.kill = True
-    return {"message": "Appointment Ended"}
+    return {"message":"Appointment Ended"}
 
 
 # Spectacles Transcript
@@ -225,63 +232,119 @@ class TranscriptRequest(BaseModel):
     appointment_id: str
     transcript: str
 
-
 @app.post("/sendTranscript")
 def sendTranscript(TranscriptRequest: TranscriptRequest):
     # if state.current_appointment != TranscriptRequest.appointment_id:
-    #     raise HTTPException(status_code=400, detail="Failure: Appointment ID mismatch")
-
+    #     raise HTTPException(status_code=400, detail="Failure: Appointment ID mismatch") 
+    
     state.transcript_text = TranscriptRequest.transcript
     state.dirty = True
     print(state.transcript_text)
-    return {"data": state.windows}
+    return {"data":state.windows}
 
 
 # RAG pipeline
 @app.post("/generateDatastore")
 def generateDatastore():
     try:
-        generate_data_store()
+        generate_data_store()   
     except Exception as e:
         print(e)
 
-    return {"message": "Database Generated Successfully"}
+    return {"message":"Database Generated Successfully"}
 
 
 # Async Agent Calls
 async def create_summary():
-    # bullet points
+    # grok or anthropic (claude)
+    chat_completion = groqClient.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "Create a quick bullet point summary of the contents: " + state.transcript_text
+            }
+        ],
+        model="llama3-8b-8192"
+    )
+    print("quick summary is the following: ")
+    print(chat_completion.choices[0].message.content)
+    state.groqSimple = chat_completion.choices[0].message.content
     return False
-
 
 async def create_detailed_summary():
-    # use perplexity
-    # ideally rag step here
-    # mix of diff things to look out for + diagnosis + analyze entire convo
+    # perplexity
+    chat_completion = groqClient.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": "You are a medical professional responsible for evaluating patients and providing appropriate care recommendations. You are tasked with assessing patient information to determine the need for further investigation or treatment. You have access to the patient's medical history, which contains detailed information about their symptoms, previous diagnoses, and treatment responses. You also have access to clinical guidelines and a medical database with evidence-based articles. Additionally, you can refer to a network of healthcare professionals for collaborative insights. Unless you are confident in your diagnosis or treatment plan, refer the patient to a specialist or another healthcare provider for further evaluation using the escalateToHuman function. Do not provide uncertain or unverified medical advice. The patient's information is as follows. Your goal is to assist the patient in achieving their health objectives to the best of your ability. Keep it concise, do not be too wordy. Give your result in 3rd person + keep it in CONCISE POINTS. : " + state.transcript_text
+            }
+        ],
+        model="llama3-8b-8192"
+    )
+    print("quick summary is the following: ")
+    print(chat_completion.choices[0].message.content)
+    state.groqDetailed = chat_completion.choices[0].message.content
     return False
 
+def conduct_background_research():
+    print("starting the conducting")
+    
+    llm = ChatOpenAI(model="gpt-4o")
 
-async def conduct_background_research():
-    # gpt call to do more research / grok
-    return False
+    transcript_text = state.groqDetailed
 
+    prompt = PromptTemplate(
+        input_variables=["transcript_text"],
+        template="Analyze these contents: {transcript_text}, tell me either True or False if this is detailed enough or if I should do more research, you can be more lenient. Again, ONLY tell me 'True' or 'False'"
+    )
+
+    # Use RunnableSequence to chain the prompt and LLM
+    chain = RunnableSequence(prompt | llm)
+
+    # Conduct research
+    detailed_summary_check = chain.invoke({"transcript_text": transcript_text})
+
+    # Check if more research is required
+    print(detailed_summary_check.content, ' is the thing byron')
+    state.moreResearchRequired = detailed_summary_check.content == "True"
+    # state.questions = detailed_summary_check.
+    print(f"{state.moreResearchRequired} is the CHECK")
+
+    return state.moreResearchRequired 
 
 async def create_dynamic_ui():
-    # anthropic llm / gpt4 (give it all info from 3 calls)
-    # smartly split it into 9 windows in specific format
-    state.windows = [{"id": 1, "data": "Bruh"}, {
-        "id": 2, "data": "Bruh"}, {"id": 3, "data": "Bruh"}]
+    # 1 - summary point form
+    # 2 - general facts from patient that will support diagnosis
+    # 3 - questions that can be asked
+    # 4 - summary of current potential diagnosis
+    # type res = {
+        # id: string;
+        # title: string;
+        # body: string;
+    #}
+    windowContent = [
+        { "id": 1, "title": "Summary point form", "body": state.groqSimple },
+        { "id": 2, "title": "General facts from the patient", "body": state.groqDetailed },
+        { "id": 3, "title": "Questions you can ask the patient", "body": state.moreResearchRequired.questions },
+        { "id": 4, "title": "Potential diagnosis", "body": state.diagnosis },
+    ]
+
+    # state.windows = [{"id":1,"data":"Bruh"},{"id":2,"data":"Bruh"},{"id":3,"data":"Bruh"}]
+    state.windows = windowContent
     return False
 
 
 async def agent_handler():
     while True:
         if (not state.dirty):
+            await asyncio.sleep(10)  # Wait for 5 seconds before checking agai
             continue
-        if (state.kill):
+
+        if(state.kill):
             print(f"Async Thread Killed")
             return
-
+        
         # Create Simple Summary
         await create_summary()
 
@@ -289,10 +352,13 @@ async def agent_handler():
         await create_detailed_summary()
 
         # Do extra Background research
-        await conduct_background_research()
+        print("beforehand byron")
+        conduct_background_research()
 
         # Use LLMs to generate UI
         await create_dynamic_ui()
 
         print(f"Updated Summaries")
-        await asyncio.sleep(5)  # Wait for 5 seconds before checking again
+        state.dirty = False
+        await asyncio.sleep(5)  # Wait for 5 seconds before checking agai
+    
